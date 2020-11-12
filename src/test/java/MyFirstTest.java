701e2b626a69dd5bb2b202ea3e60b2908b8caf37
Sjuzi <serdmane86@gmail.com>
2020-11-12 19:48:21 +0200

    initial

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class MyFirstTest {
+    Integer age = 18;
+//  BigDecimal price = new BigDecimal("10.01");
+    private int globalVar = 10;
+
+    // pishetsa vse chto ugodno
+
+    /*
+    kak zhe vse slozhno, moj mozg vishel pokuritj
+     */
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, corona, maza faka!");
+        int sum = sumTwoDigits(15, 20);
+        System.out.println("Aaaand the sum is: " + sum);
+        System.out.println(sumTwoDigits(3, 8));
+
+           }
+
+  private int sumTwoDigits(int a, int b) {
+        int c = a + b;
+        return c;
+  }
+}
